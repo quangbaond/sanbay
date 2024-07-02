8a6cd841b4e3b09614449d323c760c00016f0cee
quangbaond <quangbaorp@gmail.com>
2024-07-02 11:58:47 +0700

    develop

---
+++ resources/views/about.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+@section('content')
+<x-header title="Chi tiết" />
+<div id="main">
+    <div class="p-3">
+        <strong>Tổng công ty Cảng hàng không Việt Nam - CTCP</strong>&nbsp;(tên giao dịch quốc
+        tế:&nbsp;<strong>Airports&nbsp;Corporation of Vietnam</strong>&nbsp;- Viết tắt:&nbsp;<strong>ACV</strong>) là công
+        ty cổ phần hoạt động theo mô hình Công ty mẹ - Công ty con, được chuyển đổi từ Công ty TNHH Một thành viên do nhà
+        nước nắm giữ 100% vốn điều lệ thành công ty cổ phần do nhà nước nắm giữ cổ phần chi phối theo Quyết định số
+        1710/QĐ-TTg ngày 06 tháng 10 năm 2015 của Thủ tướng Chính phủ phê duyệt phương án cổ phần hóa Công ty mẹ - Tổng công
+        ty Cảng hàng không Việt Nam.
+        ACV hiện đang quản lý, đầu tư, khai thác khai thác hệ thống 22 Cảng&nbsp;hàng không trong cả nước, bao gồm 09 Cảng
+        hàng không quốc tế: Tân Sơn Nhất, Nội Bài, Đà&nbsp;Nẵng, Vinh, Cát Bi, Phú Bài, Cam Ranh, Phú Quốc, Cần Thơ và
+        13&nbsp;Cảng hàng không&nbsp;nội địa: Buôn Ma Thuột, Liên Khương, Rạch Giá, Cà Mau, Côn Đảo, Phù&nbsp;Cát, Pleiku,
+        Tuy Hòa, Chu Lai, Đồng Hới, Nà Sản, Điện Biên và Thọ Xuân; góp vốn vào các công ty con và công ty liên doanh, liên
+        kết.
+        ACV có vốn điều lệ 21.771.732.360.000 đồng (Hai mươi mốt nghìn bảy trăm bảy mươi mốt tỷ, bảy trăm ba mươi hai triệu,
+        ba trăm sáu mươi nghìn đồng), tương ứng 2.177.173.236 cổ phần, mệnh giá 10.000 đồng/cổ phần. Trong đó: Cổ phần Nhà
+        nước nắm giữ 95,4%; các cổ đông khác nắm giữ 4,6%.
+        <hr>
+        <div class="row">
+            <div class="col-4">
+                Tên gọi đầy đủ bằng tiếng Việt
+            </div>
+            <div class="col-8">
+                <strong>Tổng công ty Cảng hàng không Việt Nam - CTCP</strong>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                Tên viết tắt
+            </div>
+            <div class="col-8">
+                <strong>ACV</strong>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                Tên giao dịch quốc tế
+            </div>
+            <div class="col-8">
+                <strong>Airports Corporation of Vietnam</strong>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                Trụ sở chính
+            </div>
+            <div class="col-8">
+                58 đường Trường Sơn - Phường 2 - Quận Tân Bình - Thành phố Hồ Chí Minh
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-4">
+                Điện thoại
+            </div>
+            <div class="col-8">
+                (84-28) 3848 5383
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                Fax
+            </div>
+            <div class="col-8">
+                (84-28) 3844 5127
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                Website
+            </div>
+            <div class="col-8">
+                <a href="http://www.vietnamairport.vn">http://www.vietnamairport.vn</a>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                Biểu tượng
+            </div>
+            <div class="col-8">
+                <img src="https://www.vietnamairport.vn/uploads/main/users/068ad20ed7cde3007891/images/Gi%E1%BB%9Bi%20thi%E1%BB%87u/Logo%20Final%202016-02.png"
+                    alt="" width="100%" height="90">
+            </div>
+        </div>
+
+    </div>
+</div>
+@include('includes.footer')
+@endsection
+
